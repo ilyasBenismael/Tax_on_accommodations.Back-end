e0bb4a98cefef79e73fea33ec93e29e9cac91714
ilyas benismael <benismael.ilyas@gmail.com>
2023-02-24 19:15:13 +0100

    Initial commit

---
+++ src/main/java/com/example/taxe_sejour/TaxeSejourApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxe_sejour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxeSejourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxeSejourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taxe_sejour/TaxeSejourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxe_sejour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxeSejourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
