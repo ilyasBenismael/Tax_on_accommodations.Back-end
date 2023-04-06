63e3c28a59fda08fe9c43fa013c1d3dff5f68b1e
sony <nassimaaityoussef9@gmail.com>
2023-04-06 17:53:55 +0000

    nassima

--- src/main/java/com/example/taxe_sejour/dao/TrimestreDao.java
+++ src/main/java/com/example/taxe_sejour/dao/TrimestreDao.java
@@ -4,10 +4,14 @@ import com.example.taxe_sejour.bean.Trimestre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TrimestreDao extends JpaRepository<Trimestre,Long> {
     Trimestre findByNombreTrim(int nombreTrim);
     Trimestre findByRef(String ref);
+    @Override
+    List<Trimestre> findAll();
 
     Trimestre findByNombreTrimAndAnnee(int nombreTrim, int annee);
 
